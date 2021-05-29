037b1f3814f34574b500e850d49cf409583239d6
Wilson Aguilar <waguilars@est.ups.edu.ec>
2021-05-29 16:48:49 -0500

    add credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
-  token: 'BQDLk5j0JkDGUTdzYMPJuPreVgLn7z7Uyf3AeB_gMVvNktOM2-rjDCICgCAm7DOka-IyXBs1oRfVB8g4-JiiLm0mEJ5EMcagL8TofEUU-IoJwH1TTazuq7JdmzB2Ft2Ss5RJ5Ec0vn3C'
+  credentials: {
+    grant_type: 'client_credentials',
+    client_id: '183fc30fc410433685addc3038f6ac70',
+    client_secret: '2e0bba54350e43d5b64f823406ae9267',
+  },
 };
